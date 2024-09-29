515bff22df647a2be1bb82474ed4cdcb0177e085
EnisW <119161183+EnisW@users.noreply.github.com>
2024-09-29 13:36:27 +0300

    Update functions.h

--- code/functions.h
+++ code/functions.h
@@ -1,3 +1,7 @@
+#define WIFI_SSID "WIFI SSID"  // Your WIFI SSID
+#define WIFI_PASSWORD "WIFI PASSWORD"   // Your WIFI PASSWORD
+
+void sentHtml();
 void connectWifi();
 int getNumbersAndCalculate();
 void setup();
