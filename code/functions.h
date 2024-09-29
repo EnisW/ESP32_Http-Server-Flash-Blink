bb219373fbe22e1f026472b479fbb64dbb0bfd60
EnisW <119161183+EnisW@users.noreply.github.com>
2024-09-29 16:34:14 +0000

    Updated

--- code/functions.h
+++ code/functions.h
@@ -1,8 +1,17 @@
+#pragma once
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <uri/UriBraces.h>
+#include <Arduino.h>
+
 #define WIFI_SSID "WIFI SSID"  // Your WIFI SSID
 #define WIFI_PASSWORD "WIFI PASSWORD"   // Your WIFI PASSWORD
+
+#define LED1 4
 
 void sentHtml();
 void connectWifi();
 int getNumbersAndCalculate();
-void setup();
-void loop();
+
